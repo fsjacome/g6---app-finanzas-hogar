6e991d13e8ac3bba1bb094f93a5413d484e53e9d
rxcp02 <72362432+rxcp02@users.noreply.github.com>
2021-08-18 19:54:51 -0500

    Add files via upload

---
+++ objetos avanzados/vistas.sql
@@ -0,0 +1,15 @@
+DROP VIEW mostrar_ingresos;
+CREATE VIEW mostrar_gastos_ingresos AS
+SELECT ingresos.numeroCuenta, SUM(montoIngresos) AS ingresoTotales, SUM(montoGasto) AS gastosTotales
+FROM ingresos, gastos
+WHERE ingresos.numeroCuenta = gastos.numeroCuenta
+GROUP BY numeroCuenta;
+
+CREATE VIEW mostrar_balance AS
+SELECT ingresos.numeroCuenta, SUM(montoIngresos) - SUM(montoGasto) AS balance
+FROM ingresos, gastos
+WHERE ingresos.numeroCuenta = gastos.numeroCuenta
+GROUP BY numeroCuenta;
+
+SELECT * FROM mostrar_gastos_ingresos;
+SELECT * FROM mostrar_balance;
